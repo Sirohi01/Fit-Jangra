ed32a4a58bdc231ee13b3a225670a245c37f683a
Sirohi01 <126563915+Sirohi01@users.noreply.github.com>
2025-05-06 10:46:24 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Services from './pages/Services';
+import Gallery from './pages/Gallery';
+import Blog from './pages/Blog';
+import Contact from './pages/Contact';
+import Pricing from './pages/Pricing';
+import FAQ from './pages/FAQ';
+
+function App() {
+    return (
+        <Router>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/gallery" element={<Gallery />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/pricing" element={<Pricing />} />
+            <Route path="/faq" element={<FAQ />} />
+        </Routes>
+        </Router>
+    );
+}
+
+export default App;
